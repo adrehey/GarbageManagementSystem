b8070b8a4d41f9de579296a4039b74270a267ace
COLE0720 <nicoglen082007@gmail.com>
2026-04-03 22:42:01 -0700

    i dont know

--- GarbageManagementSystem/obj/Debug/net10.0-windows/GarbageManagementSystem.AssemblyInfo.cs
+++ GarbageManagementSystem/obj/Debug/net10.0-windows/GarbageManagementSystem.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("GarbageManagementSystem")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+3768750410bda54cc08e876989563296113887a9")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+33243eae213ec67f7c1df329d60fed6cebe798b7")]
 [assembly: System.Reflection.AssemblyProductAttribute("GarbageManagementSystem")]
 [assembly: System.Reflection.AssemblyTitleAttribute("GarbageManagementSystem")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
--- GarbageManagementSystem/obj/Release/net10.0-windows/GarbageManagementSystem.AssemblyInfo.cs
+++ GarbageManagementSystem/obj/Release/net10.0-windows/GarbageManagementSystem.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("GarbageManagementSystem")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Release")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+6048ef1fc2d16f0a105e8f961d732afefa96d660")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+33243eae213ec67f7c1df329d60fed6cebe798b7")]
 [assembly: System.Reflection.AssemblyProductAttribute("GarbageManagementSystem")]
 [assembly: System.Reflection.AssemblyTitleAttribute("GarbageManagementSystem")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
