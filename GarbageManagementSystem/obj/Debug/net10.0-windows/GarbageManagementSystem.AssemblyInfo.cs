8c454f7f24414489b6a773faf886777599c3e762
Enrico <nicoglen082007@gmail.com>
2026-05-21 16:15:27 +0800

    commit

--- GarbageManagementSystem/obj/Debug/net10.0-windows/GarbageManagementSystem.AssemblyInfo.cs
+++ GarbageManagementSystem/obj/Debug/net10.0-windows/GarbageManagementSystem.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("GarbageManagementSystem")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+35738a7c9e22a33b8bad1e672fe4a1987bc6b110")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+49cc421c5ab41bb7eeb996cce6cea15e001fa4e9")]
 [assembly: System.Reflection.AssemblyProductAttribute("GarbageManagementSystem")]
 [assembly: System.Reflection.AssemblyTitleAttribute("GarbageManagementSystem")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
