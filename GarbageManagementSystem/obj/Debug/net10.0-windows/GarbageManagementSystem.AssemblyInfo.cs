ceef030d605f9440c64a9d37eea9c0c908ba0f31
Andrhey Gaylan <andrheygaylan@gmail.com>
2026-05-22 23:32:05 +0800

    Pushing so I can pull

--- GarbageManagementSystem/obj/Debug/net10.0-windows/GarbageManagementSystem.AssemblyInfo.cs
+++ GarbageManagementSystem/obj/Debug/net10.0-windows/GarbageManagementSystem.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("GarbageManagementSystem")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+49cc421c5ab41bb7eeb996cce6cea15e001fa4e9")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+7fb27e62c47087ba3cd8efe6d4099784cd02b193")]
 [assembly: System.Reflection.AssemblyProductAttribute("GarbageManagementSystem")]
 [assembly: System.Reflection.AssemblyTitleAttribute("GarbageManagementSystem")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
