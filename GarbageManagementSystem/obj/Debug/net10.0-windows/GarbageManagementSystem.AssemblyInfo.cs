03849d25068c440e28b3fe1200b13421d200f966
Andrhey Gaylan <andrheygaylan@gmail.com>
2026-05-25 00:59:49 +0800

    changes idk

--- GarbageManagementSystem/obj/Debug/net10.0-windows/GarbageManagementSystem.AssemblyInfo.cs
+++ GarbageManagementSystem/obj/Debug/net10.0-windows/GarbageManagementSystem.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("GarbageManagementSystem")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+79a3e9c5a661ffeb37a2dc35b7476cf6aa992c88")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+1d64fb2aa4f659dcaec818d0b9f702706b8b1367")]
 [assembly: System.Reflection.AssemblyProductAttribute("GarbageManagementSystem")]
 [assembly: System.Reflection.AssemblyTitleAttribute("GarbageManagementSystem")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
